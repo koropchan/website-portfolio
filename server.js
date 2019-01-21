29b520b569bad75bf76994540e8ba70d8ce4bc81
Stan <koropchan.sv@gmail.com>
2019-01-21 15:01:18 -0500

    Ready for deployment

---
+++ server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 9000; //If PORT exist then use 'port' otherwise use port 9000
+
+
+app.use(express.static(path.join(__dirname, 'build'))); //Tell express to use 'build' directory
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));  // If not 'build' directory then use 'index.html'
+});
+
+app.listen(port);
