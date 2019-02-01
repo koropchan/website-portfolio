23636c434fa7e49fb439447394a836408695156b
Stan <koropchan.sv@gmail.com>
2019-01-31 23:09:18 -0500

    Add project to a projects page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,33 @@
 import React, {Component} from 'react';
 import '../styles/components/_home.scss';
 
+const API = 'https://api.github.com/users/koropchan';
+
 class Home extends Component {
+
+      //Setting initial state
+      constructor(props){
+         super(props);
+         this.state = {
+              public_repos: 0,
+            };
+
+      }
+
+      //Fetching API number of repos in Github account
+      componentDidMount() {
+        fetch(API)
+          .then(response => response.json())
+          .then(data => this.setState({ public_repos: data.public_repos }));
+      }
+      
       render() { 
+
+
           return ( 
             <div>
-                <p className="text"></p>
-                <p className="text"></p>
+                <p className="text">Hi, my name is Stan and I am a Web Developer</p>
+                <p className="text">There currently {this.state.public_repos} repositories in my Github profile</p>
                 <div className="icons">
                 <a href = "https://www.linkedin.com/in/stan-koropchan/" target="_blank" rel="noopener noreferrer"><img className="links-icons" src={require('../styles/linkedin-logo.png')} alt="icon" height="45" width="45"/></a>
                 <a href = "https://github.com/koropchan/" target="_blank" rel="noopener noreferrer"><img className="links-icons" src={require('../styles/github-logo.png')} alt="icon" height="45" width="45"/></a>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,23 @@
 import React, {Component} from 'react';
+import '../styles/components/_projects.scss';
 
 class Projects extends Component {
       render() { 
           return ( 
             <div>
-            My projects page
+              <div className="container">
+                <div id="chat" className="projects">
+                  <div class="button_div" align="center"><a class="button" href="https://mongochat-1.herokuapp.com/" target="_blank" rel="noopener noreferrer">View</a></div>
+                  <div class="button_div" align="center"><a class="button" href="https://mongochat-1.herokuapp.com/" target="_blank" rel="noopener noreferrer">Source</a></div>
+                </div>
+                <div className="projects"></div>
+                <div className="projects"></div>
+                <div className="projects"></div>
+              </div>
             </div>
            );
       }
   }
    
 export default Projects;
+
