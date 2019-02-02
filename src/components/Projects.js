c0102708f67f65a3357044eecbe2e3f3af66e83f
Stan <koropchan.sv@gmail.com>
2019-02-01 21:16:26 -0500

    Add projet to projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,8 +7,8 @@ class Projects extends Component {
             <div>
               <div className="container">
                 <div id="chat" className="projects">
-                  <div class="button_div" align="center"><a class="button" href="https://mongochat-1.herokuapp.com/" target="_blank" rel="noopener noreferrer">View</a></div>
-                  <div class="button_div" align="center"><a class="button" href="https://mongochat-1.herokuapp.com/" target="_blank" rel="noopener noreferrer">Source</a></div>
+                  <a className="links" href="https://mongochat-1.herokuapp.com/" target="_blank" rel="noopener noreferrer" align="center">View</a>
+                  <a className="links" href="https://mongochat-1.herokuapp.com/" target="_blank" rel="noopener noreferrer" align="center">Source</a>
                 </div>
                 <div className="projects"></div>
                 <div className="projects"></div>
@@ -20,4 +20,3 @@ class Projects extends Component {
   }
    
 export default Projects;
-
